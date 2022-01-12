b277bcb7bcd96e73fcd7e88808dde98ed4fdfd73
Amit Kadam <kadamamit04@gmail.com>
2022-01-12 23:45:29 +0530

    Create wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+spark-submit --name "job-name" --class com.poc.solr.hbase.Solrspark --master yarn --deploy-mode cluster jar_name.jar
+	"{	\"tables\":[
+		{
+			\"tableProp\":{
+				\"hofsPath\": \"/tmp/table_dir/\",
+				\"outputTable\": \"output_table_name\",
+				\"fieldList\": \"field_1,field_2,field_3,field_4,field_5\",
+				\"fieldslistSalr\": \"field_1,field_2,field_3,field_4,field_5\",
+				\"fieldSeq\": \"field_1,field_2,field_3,field_4,field_5\",
+				\"sortField\":\"field_id\", 
+				\"collection\:\"collection_name\",
+				\"numTasks\": 18
+				}
+		}
+	]
+	}"
